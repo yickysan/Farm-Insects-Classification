5bc720a157a05ed644641e4f4ede5828c957c0e8
Eniatorudabo Abomaye <abomayesan@gmail.com>
2023-08-07 21:49:40 +0100

    created required directories and files

---
+++ src/FarmInsectsClassification/logger.py
@@ -0,0 +1,16 @@
+import logging
+from pathlib import Path
+from datetime import datetime
+
+path = Path(__file__)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = path.getcwd() /"logs" / LOG_FILE
+logs_path.mkdir(parents=True, exist_ok=True)
+
+LOG_FILE_PATH = logs_path/ LOG_FILE
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
---
+++ template.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
+
+project_name = "FarmInsectsClassification"
+
+required_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/tests/",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py"
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "notebook/trials.ipynb"
+]
+
+for file_path in required_files:
+    file_path = Path(file_path)
+
+    # create empty directories
+    if file_path.suffix == "" and file_path.parent != Path(".github/workflows"):
+        file_dir = file_path.mkdir(parents=True, exist_ok=True) 
+
+    # split path into directory and filename
+    file_dir, file_name = file_path.parent, file_path.name 
+    
+
+    if file_dir:
+        dir_path = Path(file_dir)
+        dir_path.mkdir(parents=True, exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for the file {file_name}")
+
+    if not file_path.exists() or file_path.stat().st_size == 0:
+        file_path.touch()
+        logging.info(f"Creating empty file: {file_path}")
+
+    else:
+        logging.info(f"{file_name} already exists in {file_dir}")
+
+
+
+
+
+
